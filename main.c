beaefa8ce5e5127eb9153610cd127c3ae079cb87
2002jan <2002jan@gmail.com>
2022-04-11 10:17:04 +0200

    ups

--- main.c
+++ main.c
@@ -6,40 +6,36 @@ int main()
     initStudetsList(1000000);
     List *list = calloc(1, sizeof(List));
 
+    ListNode *current = list->first, *next;
+
     printf("Time to insert 50 items into list\n");
 
-    for (int j = 1; j <= 100; j+= 4)
+    for (int j = 1; j <= 100; j += 2)
     {
-        break;
-        int n = 5000 * j;
-
-        buildList(list, n);
+        int n = 250 * j;
 
         clock_t begin = clock();
 
-        for (int i = 0; i < 50; i++)
-        {
-            insertIntoList(list, &Students[n + i]);
-        }
+        buildList(list, n);
+
         clock_t end = clock();
         printf("%d - %fms\n", n, (double)(end - begin) / CLOCKS_PER_SEC * 1000);
+
+        list->first = NULL;
+
+        while (current != NULL)
+        {
+            next = current->next;
+            free(current);
+            current = next;
+        }
     }
-
-    ListNode *current = list->first, *next;
-
-    while (current != NULL)
-    {
-        next = current->next;
-        free(current);
-        current = next;
-    }   
 
     printf("Time to delete n items from list\n");
 
-    for (int j = 1; j <= 100; j+= 4)
+    for (int j = 1; j <= 100; j += 2)
     {
-        break;
-        int n = 200 * j;
+        int n = 250 * j;
 
         buildList(list, n);
 
@@ -47,26 +43,28 @@ int main()
 
         int i;
 
-        for(i = 0; i < n; i++){
+        for (i = 0; i < n; i++)
+        {
             toDelete[i] = i;
         }
 
         int temp, k;
 
-        for(i = n - 1; i >= 0; i--)
+        for (i = n - 1; i >= 0; i--)
         {
-             k = rand() % (n - 1);
-             temp = toDelete[k];
-             toDelete[k] = toDelete[i];
-             toDelete[i] = temp;
+            k = rand() % (n - 1);
+            temp = toDelete[k];
+            toDelete[k] = toDelete[i];
+            toDelete[i] = temp;
         }
 
         clock_t begin = clock();
 
-        for(i = 0; i < n; i++){
+        for (i = 0; i < n; i++)
+        {
             deleteFromList(list, Students[toDelete[i]].index);
         }
-        
+
         clock_t end = clock();
         printf("%d - %fms\n", n, (double)(end - begin) / CLOCKS_PER_SEC * 1000);
     }
@@ -78,13 +76,13 @@ int main()
         next = current->next;
         free(current);
         current = next;
-    }   
+    }
 
     printf("Avg time to search item from list\n");
 
-    for (int j = 1; j <= 100; j+= 4)
+    for (int j = 1; j <= 100; j += 2)
     {
-        int n = 500 * j;
+        int n = 250 * j;
 
         buildList(list, n);
 
@@ -92,12 +90,13 @@ int main()
 
         clock_t begin = clock();
 
-        for(i = 0; i < n; i++){
+        for (i = 0; i < n; i++)
+        {
             searchList(list, Students[i].index);
         }
-        
+
         clock_t end = clock();
-        printf("%d - %fms\n", n, (double)(end - begin) / CLOCKS_PER_SEC * 1000 / n);
+        printf("%d - %fms\n", n, (double)(end - begin) / CLOCKS_PER_SEC * 1000);
     }
 
     free(list);
