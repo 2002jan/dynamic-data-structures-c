cf4de86e8bb6b528445342ab976d5e3d5e4ff0cd
Jan <2002jan@gmail.com>
2022-04-24 17:21:49 +0200

    finale

--- tree.c
+++ tree.c
@@ -9,114 +9,250 @@ int main()
     initStudetsList(1000000);
     BSTNode *tree;
 
-    int *ns; 
+    int *ns;
     double *times;
 
     ns = (int *)calloc(runs, sizeof(int));
     times = (double *)calloc(runs, sizeof(double));
 
-    printf("Time to insert 50 items into list\n");
+    printf("Time to insert n items into BST\n");
 
     for (int j = 1; j <= runs; j++)
     {
         int n = 10000 * j;
-
-        clock_t begin = clock();
-
-        tree = buildTree(n);
-        tree = balanceTree(tree, n);
-
-        clock_t end = clock();
-        
         ns[j - 1] = n;
-        times[j - 1] = (double)(end - begin) / CLOCKS_PER_SEC * 1000;
+        times[j - 1] = 0;
+
+        for (int i = 0; i < 25; i++)
+        {
+            clock_t begin = clock();
+
+            tree = buildTree(n);
+
+            clock_t end = clock();
+            times[j - 1] += (double)(end - begin) / CLOCKS_PER_SEC * 1000;
+
+            clearTree(tree);
+            tree = NULL;
+        }
+
+        times[j - 1] /= 25;
 
         printf("%d - %fms\n", n, times[j - 1]);
+    }
 
-        clearTree(tree);
-        tree = NULL;
+    exportToCsv(ns, times, runs, "BST", "Insertion");
+
+    printf("Time to insert n items into BBST\n");
+
+    for (int j = 1; j <= runs; j++)
+    {
+        int n = 10000 * j;
+        ns[j - 1] = n;
+        times[j - 1] = 0;
+
+        for (int i = 0; i < 25; i++)
+        {
+            clock_t begin = clock();
+
+            tree = buildTree(n);
+            tree = balanceTree(tree, n);
+
+            clock_t end = clock();
+            times[j - 1] += (double)(end - begin) / CLOCKS_PER_SEC * 1000;
+
+            clearTree(tree);
+            tree = NULL;
+        }
+
+        times[j - 1] /= 25;
+
+        printf("%d - %fms\n", n, times[j - 1]);
     }
 
     exportToCsv(ns, times, runs, "BBST", "Insertion");
 
-    printf("Time to delete n items from list\n");
+    printf("Time to delete n items from BST\n");
 
     for (int j = 1; j <= runs; j++)
     {
         int n = 10000 * j;
-
-        tree = buildTree(n);
-        // tree = balanceTree(tree, n);
-
-        int toDelete[n];
-
-        int i;
-
-        for (i = 0; i < n; i++)
-        {
-            toDelete[i] = i;
-        }
-
-        int temp, k;
-
-        for (i = n - 1; i >= 0; i--)
-        {
-            k = rand() % (n - 1);
-            temp = toDelete[k];
-            toDelete[k] = toDelete[i];
-            toDelete[i] = temp;
-        }
-
-        clock_t begin = clock();
-
-        for (i = 0; i < n; i++)
-        {
-            tree = deleteFromTree(tree, Students[toDelete[i]].index);
-        }
-
-        clock_t end = clock();
-        
         ns[j - 1] = n;
-        times[j - 1] = (double)(end - begin) / CLOCKS_PER_SEC * 1000;
+        times[j - 1] = 0;
+
+        for (int jj = 0; jj < 25; jj++)
+        {
+            tree = buildTree(n);
+
+            int toDelete[n];
+
+            int i;
+
+            for (i = 0; i < n; i++)
+            {
+                toDelete[i] = i;
+            }
+
+            int temp, k;
+
+            for (i = n - 1; i >= 0; i--)
+            {
+                k = rand() % (n - 1);
+                temp = toDelete[k];
+                toDelete[k] = toDelete[i];
+                toDelete[i] = temp;
+            }
+
+            clock_t begin = clock();
+
+            for (i = 0; i < n; i++)
+            {
+                tree = deleteFromTree(tree, Students[toDelete[i]].index);
+            }
+
+            clock_t end = clock();
+
+            times[j - 1] += (double)(end - begin) / CLOCKS_PER_SEC * 1000;
+
+            clearTree(tree);
+            tree = NULL;
+        }
+
+        times[j - 1] /= 25;
 
         printf("%d - %fms\n", n, times[j - 1]);
-
-        clearTree(tree);
-        tree = NULL;
     }
 
     exportToCsv(ns, times, runs, "BST", "Deletion");
 
-    printf("Avg time to search item from list\n");
+    printf("Time to delete n items from BBST\n");
+
+    for (int j = 1; j <= runs; j++)
+    {
+        int n = 10000 * j;
+        ns[j - 1] = n;
+        times[j - 1] = 0;
+
+        for (int jj = 0; jj < 25; jj++)
+        {
+            tree = buildTree(n);
+            tree = balanceTree(tree, n);
+
+            int toDelete[n];
+
+            int i;
+
+            for (i = 0; i < n; i++)
+            {
+                toDelete[i] = i;
+            }
+
+            int temp, k;
+
+            for (i = n - 1; i >= 0; i--)
+            {
+                k = rand() % (n - 1);
+                temp = toDelete[k];
+                toDelete[k] = toDelete[i];
+                toDelete[i] = temp;
+            }
+
+            clock_t begin = clock();
+
+            for (i = 0; i < n; i++)
+            {
+                tree = deleteFromTree(tree, Students[toDelete[i]].index);
+            }
+
+            clock_t end = clock();
+
+            times[j - 1] += (double)(end - begin) / CLOCKS_PER_SEC * 1000;
+
+            clearTree(tree);
+            tree = NULL;
+        }
+
+        times[j - 1] /= 25;
+
+        printf("%d - %fms\n", n, times[j - 1]);
+    }
+
+    exportToCsv(ns, times, runs, "BBST", "Deletion");
+
+    printf("Avg time to search item from BST\n");
 
     for (int j = 1; j <= runs; j++)
     {
         int n = 10000 * j;
 
-        tree = buildTree(n);
-        // tree = balanceTree(tree, n);
+        ns[j - 1] = n;
+        times[j - 1] = 0;
 
-        int i;
-
-        clock_t begin = clock();
-
-        for (i = 0; i < n; i++)
+        for (int jj = 0; jj < 25; jj++)
         {
-            searchTree(tree, Students[i].index);
+            tree = buildTree(n);
+
+            int i;
+
+            clock_t begin = clock();
+
+            for (i = 0; i < n; i++)
+            {
+                searchTree(tree, Students[i].index);
+            }
+
+            clock_t end = clock();
+
+            times[j - 1] += (double)(end - begin) / CLOCKS_PER_SEC * 1000;
+
+            clearTree(tree);
+            tree = NULL;
         }
 
-        clock_t end = clock();
-        
-        ns[j - 1] = n;
-        times[j - 1] = (double)(end - begin) / CLOCKS_PER_SEC * 1000;
+        times[j - 1] /= 25;
 
         printf("%d - %fms\n", n, times[j - 1]);
-
-        clearTree(tree);
-        tree = NULL;
     }
 
     exportToCsv(ns, times, runs, "BST", "Search");
+
+    printf("Avg time to search item from BBST\n");
+
+    for (int j = 1; j <= runs; j++)
+    {
+        int n = 10000 * j;
+
+        ns[j - 1] = n;
+        times[j - 1] = 0;
+
+        for (int jj = 0; jj < 25; jj++)
+        {
+            tree = buildTree(n);
+            tree = balanceTree(tree, n);
+
+            int i;
+
+            clock_t begin = clock();
+
+            for (i = 0; i < n; i++)
+            {
+                searchTree(tree, Students[i].index);
+            }
+
+            clock_t end = clock();
+
+            times[j - 1] += (double)(end - begin) / CLOCKS_PER_SEC * 1000;
+
+            clearTree(tree);
+            tree = NULL;
+        }
+
+        times[j - 1] /= 25;
+
+        printf("%d - %fms\n", n, times[j - 1]);
+    }
+
+    exportToCsv(ns, times, runs, "BBST", "Search");
 
     free(tree);
     free(Students);
